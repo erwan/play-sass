2a66715018d6d9dc524d76e06b5ee1471754f43d
Erwan Loisant <eloisant@gmail.com>
2012-12-17 17:18:00 +0100

    Progress on sass

--- src/main/scala/SassCompiler.scala
+++ src/main/scala/SassCompiler.scala
@@ -1,4 +1,4 @@
-package net.litola
+package net.caffeinelab
 
 import sbt.PlayExceptions.AssetCompilationException
 import java.io.File
@@ -12,43 +12,59 @@ import scala.collection.JavaConversions._
 object SassCompiler {
 
   // Don't reuse for thread safety
-  def scriptingContainer = {
+  def scriptingContainer(loadPaths: Seq[String]) = {
       val container = new ScriptingContainer(org.jruby.embed.LocalContextScope.SINGLETON)
-      // container.getProvider().setLoadPaths();
+      container.getProvider().setLoadPaths(loadPaths)
       container
   }
 
-  def compile(sassFile: File, options: Seq[String]): (String, Option[String], Seq[File]) = {
-    println("Compiling: " + sassFile)
-    val cssOutput = compile(sassFile, false)
-    val compressedCssOutput = compile(sassFile, true)
+  def compile(sassFile: File, loadPaths: Seq[String]): (String, Option[String], Seq[File]) = {
+    val cssOutput = compile(sassFile, loadPaths, false)
+    val compressedCssOutput = compile(sassFile, loadPaths, true)
     (cssOutput, Some(compressedCssOutput), dependencies(sassFile))
   }
 
-  def compile(sassFile: File, compressed: Boolean): String = {
-    val result = new StringBuffer();
-    scriptingContainer.put("@result", result);
-    scriptingContainer.runScriptlet("""
-      require 'sass'
-      require 'compass'
+  val ErrorPattern = """.*Sass\:\:SyntaxError\:\s(.*)\n.*""".r
 
-      @result.append("boo")
-""") /*
-      options = {}
-      options[:load_paths] = ${sassFile.getAbsolutePath()}
-      options[:update] = true
-      options[:style] = ${if (compressed) "compressed" else "expanded"}
-      options[:line_comments] = ${if (compressed) "false" else "true"}
-      options[:syntax] = ${if (sassFile.getAbsolutePath().endsWith(".scss")) ":scss" else ":sass"}
-      input = File.new('${css.getAbsolutePath()}', 'r')
-      tree = ::Sass::Engine.new(input.read(), options).to_tree
-      @result.append(tree.render)
-    """)*/
-    result.toString
+  def compile(sassFile: File, loadPaths: Seq[String], compressed: Boolean): String = {
+    val errors = new java.io.StringWriter()
+    val result = new StringBuffer()
+    val includes = Seq(sassFile.getAbsolutePath())
+    val container = scriptingContainer(loadPaths)
+    container.setErrorWriter(errors)
+    container.put("@result", result)
+    try {
+      container.runScriptlet("""
+        require 'sass'
+        require 'compass'
 
+        options = {}
+        options[:load_paths] = %LOADPATH%
+        options[:update] = true
+        options[:style] = %STYLE%
+        options[:line_comments] = '%COMMENTS%'
+        options[:syntax] = %SYNTAX%
+        input = File.new('%ABSPATH%', 'r')
+        tree = ::Sass::Engine.new(input.read(), options).to_tree
+        @result.append(tree.render)
+      """.replace("%LOADPATH%", "[" + includes.map("'" + _ + "'").mkString(",") + "]")
+         .replace("%ABSPATH%", sassFile.getAbsolutePath())
+         .replace("%STYLE%", if (compressed) ":compressed" else ":nested")
+         .replace("%COMMENTS%", if (compressed) "false" else "true")
+         .replace("%SYNTAX%", if (sassFile.getAbsolutePath().endsWith(".scss")) ":scss" else ":sass"))
+      result.toString
+    } catch {
+      case e: Exception =>
+        val sassError: String = (ErrorPattern findAllIn errors.toString).toSeq.headOption.getOrElse("")
+        throw new AssetCompilationException(Some(sassFile),
+          "SASS Compilation of " + sassFile + " failed: " + sassError,
+          Some(0), // TODO: real line number (if possible)
+          None)
+    }
   }
 
-  private def dependencies(sass: File): Seq[File] = Nil
+  // TODO: Dependency management
+  private def dependencies(sass: File): Seq[File] = Seq(sass)
 
 }
 
--- src/main/scala/SassPlugin.scala
+++ src/main/scala/SassPlugin.scala
@@ -1,25 +1,102 @@
-package net.litola
+package net.caffeinelab
 
 import sbt._
 import sbt.Keys._
-import PlayProject._
+import play._
+import PlayExceptions._
+
 
 object SassPlugin extends Plugin {
   val sassEntryPoints = SettingKey[PathFinder]("play-sass-entry-points")
   val sassOptions = SettingKey[Seq[String]]("play-sass-options")
-  val sassWatcher = PlayProject.AssetsCompiler("sass",
-    { file => (file ** "*.sass") +++ (file ** "*.scss") },
-    sassEntryPoints,
-    { (name, min) =>
-      name.replace(".sass", if (min) ".min.css" else ".css")
-      name.replace(".scss", if (min) ".min.css" else ".css")
-    },
-    { SassCompiler.compile _ },
-    sassOptions)
+
+  private def expand(directories: Seq[File]): Seq[File] = directories.flatMap { _ match {
+    case f if f.isDirectory => Seq(f) ++ expand(f.listFiles)
+    case f => Nil
+  }}
 
   override val settings = Seq(
     sassEntryPoints <<= (sourceDirectory in Compile)(base => ((base / "assets" ** "*.sass") +++ (base / "assets" ** "*.scss") --- base / "assets" ** "_*")),
     sassOptions := Seq.empty[String],
-    resourceGenerators in Compile <+= sassWatcher)
+    resourceGenerators in Compile <+= (target, state, sourceDirectory in Compile, resourceManaged in Compile, cacheDirectory, streams, sassEntryPoints) map { (target, state, src, resources, cache, streams, files) =>
+
+      val logger = streams.log
+      val sassWorkingDir = target / "sass-compass"
+
+      if (!sassWorkingDir.exists) {
+        val maybeSass = this.getClass.getClassLoader.asInstanceOf[java.net.URLClassLoader].getURLs.map(_.getFile).map(file).find { file =>
+          file.getAbsolutePath.contains("jruby-complete")
+        }
+
+        maybeSass.map { zipFile =>
+          IO.unzip(zipFile, sassWorkingDir)
+        }.orElse {
+          sys.error("OOPS. missing SASS?")
+        }
+      }
+      // val loadPaths = expand(Seq(sassWorkingDir / "gems")).map(_.getAbsolutePath)
+      val loadPaths = (sassWorkingDir / "gems").listFiles.map(_ / "lib").map(_.getAbsolutePath)
+
+      // Here `sassWorkingDir` reference a directory containing the whole SASS Stuff, so you can work with it as process resources as usual.
+      import java.io._
+
+      val name = "sass"
+      val naming:  (String, Boolean) => String = (name, min) => name.replaceAll(".s[ac]ss", if (min) ".min.css" else ".css")
+      val watch: File => PathFinder = (file => (file ** "*.sass") +++ (file ** "*.scss"))
+      val cacheFile = cache / name
+      val currentInfos = watch(src).get.map(f => f -> FileInfo.lastModified(f)).toMap
+
+      val (previousRelation, previousInfo) = Sync.readInfo(cacheFile)(FileInfo.lastModified.format)
+
+      if (previousInfo != currentInfos) {
+
+        //a changed file can be either a new file, a deleted file or a modified one
+        lazy val changedFiles: Seq[File] = currentInfos.filter(e => !previousInfo.get(e._1).isDefined || previousInfo(e._1).lastModified < e._2.lastModified).map(_._1).toSeq ++ previousInfo.filter(e => !currentInfos.get(e._1).isDefined).map(_._1).toSeq
+
+        //erease dependencies that belong to changed files
+        val dependencies = previousRelation.filter((original, compiled) => changedFiles.contains(original))._2s
+        dependencies.foreach(IO.delete)
+
+        /**
+         * If the given file was changed or
+         * if the given file was a dependency,
+         * otherwise calculate dependencies based on previous relation graph
+         */
+        val generated: Seq[(File, java.io.File)] = (files x relativeTo(Seq(src / "assets"))).flatMap {
+          case (sourceFile, name) => {
+            if (changedFiles.contains(sourceFile) || dependencies.contains(new File(resources, "public/" + naming(name, false)))) {
+              val (debug, min, deps) = try {
+                SassCompiler.compile(sourceFile, loadPaths)
+              } catch {
+                case e: AssetCompilationException => throw play.Project.reportCompilationError(state, e)
+              }
+              val out = new File(resources, "public/" + naming(name, false))
+              IO.write(out, debug)
+              (deps ++ Seq(sourceFile)).toSet[File].map(_ -> out) ++ min.map { minified =>
+                val outMin = new File(resources, "public/" + naming(name, true))
+                IO.write(outMin, minified)
+                deps.map(_ -> outMin)
+              }.getOrElse(Nil)
+            } else {
+              previousRelation.filter((original, compiled) => original == sourceFile)._2s.map(sourceFile -> _)
+            }
+          }
+        }
+
+        //write object graph to cache file 
+        Sync.writeInfo(cacheFile,
+          Relation.empty[File, File] ++ generated,
+          currentInfos)(FileInfo.lastModified.format)
+
+        // Return new files
+        generated.map(_._2).distinct.toList
+
+      } else {
+        // Return previously generated files
+        previousRelation._2s.toSeq
+      }
+
+    })
+  
 }
 
